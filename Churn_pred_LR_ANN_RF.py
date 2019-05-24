3e892080865ed0684722eb27300c67393b5a1b3c
Iaro <iaroslav.sedrichev@gmail.com>
2019-05-24 14:23:16 -0700

    Working: added a K-fold cross validation

--- Churn_pred_LR_ANN_RF.py
+++ Churn_pred_LR_ANN_RF.py
@@ -44,6 +44,48 @@ X_test = sc.transform(X_test)
 
 
 
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy',max_depth = 6 , random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred_Test_proba = classifier.predict_proba(X_test)
+
+y_pred_Train_proba = classifier.predict_proba(X_train)
+
+CAP = np.stack((y_train,y_pred_Train_proba[:,1]), axis=1)
+
+CAP = pd.DataFrame({'y_train' : CAP[:,0],
+                    'y_train_proba' : CAP[:,1]}).sort_values(by=['y_train_proba'], ascending =False).to_csv(r"C:\Users\iasedric.REDMOND\Documents\_Perso\Training\Data Science A-Z Template Folder\Churn\For_CAP_wRegressionForest.csv", index=False, encoding='utf_8_sig')
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm_RF = confusion_matrix(y_test, y_pred)
+
+
+#Applying K-fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+
+mean = accuracies.mean()
+std = accuracies.std()
+
+
+CAP = np.stack((y_test,y_pred_Test_proba[:,1]), axis=1)
+
+CAP = pd.DataFrame({'y_test' : CAP[:,0],
+                    'y_test_proba' : CAP[:,1]}).sort_values(by=['y_test_proba'], ascending =False).to_csv(r"C:\Users\iasedric.REDMOND\Documents\_Perso\Training\Data Science A-Z Template Folder\Churn\For_CAP_wRegressionForest_test.csv", index=False, encoding='utf_8_sig')
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 14:15:54 2019
+
+@author: iasedric
+"""
+
 
 """
 # Fitting Logistic Regression to the Training set
@@ -127,37 +169,5 @@ CAP = np.stack((y_test,y_pred_Test_proba[:,0]), axis=1)
 CAP = pd.DataFrame({'y_test' : CAP[:,0],
                     'y_test_proba' : CAP[:,1]}).sort_values(by=['y_test_proba'], ascending =False).to_csv(r"C:\Users\iasedric.REDMOND\Documents\_Perso\Training\Data Science A-Z Template Folder\Churn\For_CAP_wANN_test.csv", index=False, encoding='utf_8_sig')
 
+
 """
-
-
-# Fitting Random Forest Classification to the Training set
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy',max_depth = 6 , random_state = 0)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred_Test_proba = classifier.predict_proba(X_test)
-
-y_pred_Train_proba = classifier.predict_proba(X_train)
-
-CAP = np.stack((y_train,y_pred_Train_proba[:,1]), axis=1)
-
-CAP = pd.DataFrame({'y_train' : CAP[:,0],
-                    'y_train_proba' : CAP[:,1]}).sort_values(by=['y_train_proba'], ascending =False).to_csv(r"C:\Users\iasedric.REDMOND\Documents\_Perso\Training\Data Science A-Z Template Folder\Churn\For_CAP_wRegressionForest.csv", index=False, encoding='utf_8_sig')
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm_RF = confusion_matrix(y_test, y_pred)
-
-
-
-
-
-CAP = np.stack((y_test,y_pred_Test_proba[:,1]), axis=1)
-
-CAP = pd.DataFrame({'y_test' : CAP[:,0],
-                    'y_test_proba' : CAP[:,1]}).sort_values(by=['y_test_proba'], ascending =False).to_csv(r"C:\Users\iasedric.REDMOND\Documents\_Perso\Training\Data Science A-Z Template Folder\Churn\For_CAP_wRegressionForest_test.csv", index=False, encoding='utf_8_sig')
-
-
-
